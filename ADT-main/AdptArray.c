bbc7b9b2b45b83dc4642930241e3af539f566eff
Sari <sygw6457@gmail.com>
2023-03-15 18:46:28 +0200

    initialization

---
+++ ADT-main/AdptArray.c
@@ -0,0 +1,37 @@
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "AdptArray.h"
+
+// struct structure 
+typedef struct AdptArray_
+{
+	COPY_FUNC cf;
+	PRINT_FUNC pf;
+	DEL_FUNC df;
+	int SIZE_ARR;
+}AdptArray, * PAdptArray;
+
+
+
+
+PAdptArray CreateAdptArray(COPY_FUNC cf, DEL_FUNC df, PRINT_FUNC pf)
+{
+	// space allocation
+	PAdptArray new_arr;
+	new_arr = (PAdptArray)malloc(sizeof(AdptArray) * 2);
+	if (new_arr == NULL)
+	{
+		// If no place has been allocated
+		prift("%d", " alloaction faild ");
+		exit(FAIL);
+	}
+	// Initialize values
+	new_arr->cf = cf;
+	new_arr->df = df;
+	new_arr->pf = pf;
+	new_arr->SIZE_ARR = 0;
+
+	return new_arr;
+}
