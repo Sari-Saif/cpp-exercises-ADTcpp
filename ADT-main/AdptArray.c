1c705056d15c75da1c031c3198f236596cb712c5
Sari <sygw6457@gmail.com>
2023-03-15 19:25:46 +0200

    refactoring

--- ADT-main/AdptArray.c
+++ ADT-main/AdptArray.c
@@ -10,6 +10,7 @@ typedef struct AdptArray_
 	COPY_FUNC cf;
 	PRINT_FUNC pf;
 	DEL_FUNC df;
+	PElement *P_E;
 	int SIZE_ARR;
 }AdptArray, * PAdptArray;
 
@@ -24,13 +25,14 @@ PAdptArray CreateAdptArray(COPY_FUNC cf, DEL_FUNC df, PRINT_FUNC pf)
 	if (new_arr == NULL)
 	{
 		// If no place has been allocated
-		prift("%d", " alloaction faild ");
+		prift(" alloaction faild ");
 		exit(FAIL);
 	}
 	// Initialize values
 	new_arr->cf = cf;
 	new_arr->df = df;
 	new_arr->pf = pf;
+	new_arr->P_E = NULL;
 	new_arr->SIZE_ARR = 0;
 
 	return new_arr;
